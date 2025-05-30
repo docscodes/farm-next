74df2671ae4d7b03992ab70b13e70b36ddd1c216
docscodes <docscodes@gmail.com>
2025-05-30 02:00:59 -0400

    login form

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,5 +1,12 @@
+import LoginForm from "@/components/LoginForm";
+
 const page = () => {
-  return <div>login</div>;
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <LoginForm />
+    </div>
+  );
 };
 
 export default page;
